6c2d06c1907793b11cee9454fe9b25e1bc7bc1ed
mihirpP13 <mihir.prajapati@artemhealthtech.com>
2026-06-25 12:48:55 +0530

    Revert "Feat:"

--- artem_hrms/public/js/employee.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Employee", {
-	refresh: function (frm) {
-		frm.set_df_property("holiday_list", "hidden", 0);
-	},
-});
