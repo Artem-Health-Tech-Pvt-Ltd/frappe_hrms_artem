aa54816aba3f43346a4550d975a7778545f7009b
Mihir <mihir.prajapati@artemhealthtech.com>
2026-08-31 18:26:25 +0530

    removed unnecessary code

--- artem_hrms/hooks.py
+++ artem_hrms/hooks.py
@@ -144,13 +144,9 @@ doctype_js = {
 
 doc_events = {
     "Employee": {
-        "validate": "artem_hrms.facebio_integration.employee_sync.validate_employee_for_facebio",
         "on_update": [
             "artem_hrms.doc_events.employee.update_administrative_officer_permissions",
-            "artem_hrms.api.employee_device_id.on_employee_update",
-            "artem_hrms.facebio_integration.employee_sync.enqueue_employee_sync"
-        ],
-        'after_save': 'artem_hrms.facebio_integration.employee_sync.enqueue_employee_sync'
+        ]
     },
     "Employee Checkin": {
         "validate": "artem_hrms.doc_events.employee_checkin.employee_validation"
